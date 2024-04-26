fd7d241ae5ca7dad352bed6f4b6dc39869329586
Shruthi Niranjan <shruthi.dharnaik@testingxperts.com>
2024-04-26 16:45:03 +0530

    Adding files for service support for opportunity

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -28,7 +28,7 @@ public class Hooks {
     String imagePath;
     String pathForLogger;
 
-    @Before("@Web or @salesforce")  //@Amazon
+    @Before("@Web or @salesforce or @salesforce_servicesupport")  //@Amazon
     public void beforeMethodAmazon(Scenario scenario) {
 
         if (scenario.getName().contains("_"))
